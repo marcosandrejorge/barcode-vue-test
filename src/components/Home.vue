09e7b88f9f87ac11e8371d40d542935812ddffe7
Marcos André Jorge <marcosandre.bnu@gmail.com>
2021-03-24 16:59:16 -0300

    Teste com plugin

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,44 @@
 <template>
   <div>
-    <p>Teste</p>
+    <p>{{loading}}</p>
   </div>
-    
 </template>
 
 <script>
 export default {
-
-}
+  data: () => ({
+    loading: false,
+  }),
+  created() {
+    // Pass an options object with `eventBus: true` to receive an eventBus back
+    // which emits `start` and `finish` events
+    const eventBus = this.$barcodeScanner.init(this.onBarcodeScanned, {
+      eventBus: true,
+    });
+    if (eventBus) {
+      eventBus.$on("start", () => {
+        this.loading = true;
+      });
+      eventBus.$on("finish", () => {
+        this.loading = false;
+      });
+    }
+  },
+  destroyed() {
+    // Remove listener when component is destroyed
+    this.$barcodeScanner.destroy();
+  },
+  methods: {
+    // Create callback function to receive barcode when the scanner is already done
+    onBarcodeScanned(barcode) {
+      console.log(barcode);
+      // do something...
+    },
+    // Reset to the last barcode before hitting enter (whatever anything in the input box)
+    resetBarcode() {
+      let barcode = this.$barcodeScanner.getPreviousCode();
+      // do something...
+    },
+  },
+};
 </script>
